9e91a3574fc0b22e65de9dd03bd88ecac44af326
Ilham Maulana <aammaul7@gmail.com>
2024-08-31 10:14:14 +0700

    [fitur] add projects page

--- src/routers/route.tsx
+++ src/routers/route.tsx
@@ -1,5 +1,6 @@
 import Layout from "@/components/templates/layouts";
 import HomePage from "@/pages/HomePage";
+import ProjectsPage from "@/pages/ProjectsPage";
 import { createBrowserRouter } from "react-router-dom";
 
 export const router = createBrowserRouter([
@@ -7,5 +8,9 @@ export const router = createBrowserRouter([
 		path: "/",
 		element: <Layout />,
 		children: [{ path: "/", element: <HomePage /> }],
+	},
+	{
+		path: "/projects",
+		children: [{ path: "/projects", element: <ProjectsPage /> }],
 	},
 ]);
